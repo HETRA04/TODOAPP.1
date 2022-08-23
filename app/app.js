06a11ed8ee28cd44fac61ac5860cac83096a75f6
HETRA04 <abdulla.arteh@gmail.com>
2022-08-23 19:17:53 +0100

    put functional apps into folder

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const taskname = document.getElementById("addtask");
+
+
+
+
+function addtask(){
+    // add task from bar to unordered list
+    document.getElementById("submit").onclick = function(){
+        const taskname = document.getElementById("addtask");
+        console.log(taskname)
+        
+
+    }
+}
