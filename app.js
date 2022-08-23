b90ec038f8d3659de6b63414206fc61f4165b2ed
HETRA04 <abdulla.arteh@gmail.com>
2022-08-23 21:27:01 +0100

    Added functionality to the system

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const taskname = document.getElementById("addtask");
+// variables for buttons, input and containers
+const input = document.getElementById("addtext");
+const todocontainer = document.getElementById("myul") ;
+const submit = document.getElementById("submitbtn");
+const remove = document.getElementById("clearbtn");
 
 
 
 
-function addtask(){
-    // add task from bar to unordered list
-    document.getElementById("submit").onclick = function(){
-        const taskname = document.getElementById("addtask");
-        console.log(taskname)
-        
-
+submit.addEventListener('click', function(){
+    // prevent submit button if input is empty
+    
+    /* add list component and a remove button for
+    individual list components*/ 
+    var li = document.createElement('li');
+    li.innerText = input.value;
+    var but = document.createElement('button');
+    but.innerText = "remove";
+    todocontainer.appendChild(li);
+    todocontainer.appendChild(but);
+    but.onclick = function(){
+        todocontainer.removeChild(li);
+        todocontainer.removeChild(but);
     }
-}
+    // clear all task button
+    
+    input.value = ""
+
+})
+
+
+
+
