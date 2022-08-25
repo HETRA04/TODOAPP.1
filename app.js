25858e7e9f61cb8701afbafc46e87288a820ecc6
HETRA04 <abdulla.arteh@gmail.com>
2022-08-25 13:32:02 +0100

    added styling

--- app.js
+++ app.js
@@ -7,30 +7,41 @@ const remove = document.getElementById("clearbtn");
 
 
 
+
 submit.addEventListener('click', function(){
     // prevent submit button if input is empty
-    /* if(input.value == ""){
-        submit.disabled = true;
-    }
-    else{
-        submit.disabled = false;
-    } */
+    
     /* add list component and a remove button for
     individual list components*/ 
     var li = document.createElement('li');
     li.innerText = input.value;
     var but = document.createElement('button');
     but.innerText = "remove";
+    var checker = document.createElement('input')
+    checker.type = "checkbox"
     todocontainer.appendChild(li);
     todocontainer.appendChild(but);
+    todocontainer.appendChild(checker);
+    li.id = "li"
+    but.id = "but"
+    checker.id = "checker"
     but.onclick = function(){
         todocontainer.removeChild(li);
         todocontainer.removeChild(but);
+        todocontainer.removeChild(checker);
     }
-    // clear all task button
     
+    
+    
+    
+    
+
     input.value = ""
 
+})
+remove.addEventListener('click', function(){
+    
+    todocontainer.reset();
 })
 
 
